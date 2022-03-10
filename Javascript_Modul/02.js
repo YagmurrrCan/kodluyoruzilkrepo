1c504269116c586a71fb302b8719efef65d7176f
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-03-10 23:45:49 +0300

    Create

---
+++ Javascript_Modul/02.js
@@ -0,0 +1,31 @@
+// var serverName = "kodluyoruz.org";
+// console.log(serverName);
+
+
+let serverName;
+serverName = "https://kodluyoruz.org"
+console.log(serverName);
+
+let password = "1234";
+console.log(password); 
+
+
+let fullName = "Yağmur Can";
+console.log(fullName);
+
+
+console.log(fullName = fullName + " EKLEME BU KADAR BASİT YAPILABİLİYOR");
+console.log(fullName);
+
+fullName = "sıfırlandı";
+fullName += " ve yeni bilgi eklendi.";
+console.log(fullName);
+
+//const boş olamaz daima yanında tanımı olmalı. let boş olabilir. zB: let serverName
+const serverPassword = "hbjıhb";
+console.log(serverPassword);
+
+// BİR ŞEY DEĞİŞMEYECEKSE "CONST". ÖRNEĞİN TC KİMLİK NO.
+// ÇÜNKÜ DEĞİŞMİYOR.
+// MESELA serverPassword = "2355"; console.log(serverPassword) HATA VERİR ÇÜNKÜ BİR DEFA TANIMLANDI!
+
